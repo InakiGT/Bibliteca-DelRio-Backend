cdd49be484c0b94238bf6416a442cd8893db6b34
Iñaki García <yarimfuego@gmail.com>
2022-12-01 20:04:55 -0600

    CORS config added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const config = require('./config/config');
 const routersApi = require('./routes');
 const { Errors } = require('./middlewares/error.handler');
@@ -6,6 +7,18 @@ require('./utils/auth');
 
 const app = express();
 
+const whitelist = [ 'http://127.0.0.1:5500' ];
+const options = {
+    origin: ( origin, callback ) => {
+        if( whitelist.includes(origin) || !origin ) {
+            callback( null, true );
+        } else {
+            callback(new Error('Not allowed'));
+        }
+    }
+}
+
+app.use(cors(options));
 app.use(express.json());
 app.use('/new_page', ( _, res ) => {
     res.json({ msg: "Bienvenido a la biblioteca virtual DelRio" })
